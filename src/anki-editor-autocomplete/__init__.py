b432af4e63047fe388f43fc471a32d3abcd1e5ff
RisingOrange <jakub.fidler@protonmail.com>
2021-08-12 17:01:00 +0200

    Working version for 2.1.46

--- src/anki-editor-autocomplete/__init__.py
+++ src/anki-editor-autocomplete/__init__.py
@@ -1,113 +1,133 @@
 # https://github.com/sartak/anki-editor-autocomplete
 # -*- coding: utf8 -*-
 
+import json
 import re
-from aqt import mw
-from aqt import editor
-from aqt.fields import FieldDialog
-from anki.hooks import wrap
-from anki.utils import splitFields, stripHTMLMedia
-from anki.utils import json
-import urllib.request, urllib.error, urllib.parse
+
+from aqt import gui_hooks
+from aqt.editor import Editor
 
 RE_SUB_INVALID = re.compile(r"[^a-zA-Z0-9 \n\s]").sub
 
 noAutocompleteFields = []
+prevAutocomplete = ""
 
-def mySetup(self, note, hide=True, focus=False):
-    self.prevAutocomplete = ""
+def setup(js, note, editor):
+    global prevAutocomplete
+    prevAutocomplete = ""
 
     # only initialize the autocompleter on Add Cards not in browser
-    if self.note and self.addMode:
-        self.web.eval(
-            """
-            document.styleSheets[0].addRule('.autocomplete', 'margin: 0.3em 0 1.0em 0; color: blue; text-decoration: underline; cursor: pointer;');
+    # if editor.note and editor.addMode:
 
-            // every second send the current state over
-            setInterval(function () {
-                if (currentField) {
-                    var r = {
-                        text: currentField.innerHTML,
-                    };
+    my_js = """
+        document.styleSheets[1].addRule('.autocomplete', 'margin: 0.3em 0 1.0em 0; color: blue; text-decoration: underline; cursor: pointer;');
 
-                    pycmd("autocomplete:" + JSON.stringify(r));
-                }
-            }, 1000);
-        """
-        )
-
-
-def myBridge(self, cmd, _old=None):
-    if cmd.startswith("autocomplete"):
-        (type, jsonText) = cmd.split(":", 1)
-        result = json.loads(jsonText)
-        text = self.mungeHTML(result["text"])
-
-        # Work-around: delete all symbols from the search
-        text = RE_SUB_INVALID("", text)
-
-        if self.currentField is None:
-            return
-        # bail out if the user hasn't actually changed the field
-        previous = "%d:%s" % (self.currentField, text)
-        if self.prevAutocomplete == previous:
-            return
-        self.prevAutocomplete = previous
-
-        if text == "" or len(text) > 500 or self.note is None:
-            self.web.eval("$('.autocomplete').remove();")
-            return
-
-        field = self.note.model()["flds"][self.currentField]
-
-        if field["name"] in noAutocompleteFields:
-            field["no_autocomplete"] = True
-
-        if "no_autocomplete" in list(field.keys()) and field["no_autocomplete"]:
-            return
-
-        # find a value from the same model and field whose
-        # prefix is what the user typed so far
-        model_name = self.note.model()["name"]
-        field_name = field["name"]
-        query = f'note:"{model_name}" "{field_name}:{text}*"'
-        # query = "'note:%s' '%s:%s*'" % (
-        # self.note.model()['name'],
-        # field['name'],
-        # text)
-
-        col = self.note.col
-        res = col.find_cards(query, order=True)
-
-        if len(res) == 0:
-            self.web.eval("$('.autocomplete').remove();")
-            return
-
-        # pull out the full value
-        value = col.getCard(res[0]).note().fields[self.currentField]
-
-        escaped = json.dumps(value)
-
-        self.web.eval(
-            """
-            $('.autocomplete').remove();
-
+        // every second send the current state over
+        setInterval(function () {
+            const currentField = getCurrentField()
             if (currentField) {
-		$('<div class="autocomplete">' + %s + '</div>').click({field: currentField}, updateField).insertAfter(currentField)
+                console.log(currentField.editable.innerHTML)
+                var r = {
+                   text: currentField.editable.innerHTML
+                };
+                pycmd("autocomplete:" + JSON.stringify(r));
             }
+        }, 1000);
+    """
+    return js + my_js
 
-	    function updateField(event){
-                currentField = event.data.field;
-                currentField.innerHTML = %s;
-                saveField("key");
-                focusField(currentFieldOrdinal());
-                caretToEnd();
-	    }
+
+def handle_bridge_command(handled, cmd, context):
+    if not isinstance(context, Editor):
+        return handled
+
+    if not cmd.startswith("autocomplete"):
+        return handled
+
+    global prevAutocomplete
+
+    editor: Editor = context
+
+    (type, jsonText) = cmd.split(":", 1)
+    result = json.loads(jsonText)
+    text = editor.mungeHTML(result["text"])
+
+    # Work-around: delete all symbols from the search
+    text = RE_SUB_INVALID("", text)
+
+    if editor.currentField is None:
+        return handled
+
+    # bail out if the user hasn't actually changed the field
+    previous = "%d:%s" % (editor.currentField, text)
+    if prevAutocomplete == previous:
+        return handled
+    prevAutocomplete = previous
+
+    if text == "" or len(text) > 500 or editor.note is None:
+        editor.web.eval("$('.autocomplete').remove();")
+        return handled
+
+    field = editor.note.note_type()["flds"][editor.currentField]
+
+    if field["name"] in noAutocompleteFields:
+        field["no_autocomplete"] = True
+
+    if "no_autocomplete" in list(field.keys()) and field["no_autocomplete"]:
+        return handled
+
+    # find a value from the same model and field whose
+    # prefix is what the user typed so far
+    model_name = editor.note.note_type()["name"]
+    field_name = field["name"]
+    query = f'note:"{model_name}" "{field_name}:{text}*"'
+    # query = "'note:%s' '%s:%s*'" % (
+    # editor.note.model()['name'],
+    # field['name'],
+    # text)
+
+    col = editor.note.col
+    res = col.find_cards(query, order=True)
+
+    if len(res) == 0:
+        editor.web.eval("$('.autocomplete').remove();")
+        return handled
+
+    # pull out the full value
+    value = col.getCard(res[0]).note().fields[editor.currentField]
+
+    escaped = json.dumps(value)
+
+    print(res)
+    print(value)
+    print(escaped)
+    print()
+
+    editor.web.eval(
         """
-            % (escaped, escaped)
-        )
-    else:
-        _old(self, cmd)
+        $('.autocomplete').remove();
+
+        (function(){
+            let currentField = getCurrentField()
+            if (currentField) {
+            $('<div class="autocomplete">' + %s + '</div>').click({field: currentField}, updateField).insertAfter(currentField)}
+
+            function updateField(event){
+                currentField = event.data.field;
+                console.log(currentField)
+                console.log(currentField.editable)
+                currentField.editable.fieldHTML = %s;
+
+                console.log(currentField.ord)
+                focusField(currentField.ord);
+                currentField.editable.caretToEnd();
+            }
+        })()
+    """
+        % (escaped, escaped)
+    )
+
+    return handled
 
 
 # XXX must figure out how to add noAutocomplete checkbox to form
@@ -128,8 +148,9 @@ def mySaveField(self):
     fld["no_autocomplete"] = f.noAutocomplete.isChecked()
 
 
-editor.Editor.onBridgeCmd = wrap(editor.Editor.onBridgeCmd, myBridge, "around")
-editor.Editor.setNote = wrap(editor.Editor.setNote, mySetup, "after")
+# editor.Editor.onBridgeCmd = wrap(editor.Editor.onBridgeCmd, myBridge, "around")
+gui_hooks.webview_did_receive_js_message.append(handle_bridge_command)
+gui_hooks.editor_will_load_note.append(setup)
 
 # FieldDialog.loadField = wrap(FieldDialog.loadField, myLoadField, 'after')
 # FieldDialog.saveField = wrap(FieldDialog.saveField, mySaveField, 'after')
