1076f41958d92aefd1efe7eebafafb79e9ae1c08
RisingOrange <jakub.fidler@protonmail.com>
2021-08-16 13:01:41 +0200

    Refactor

--- src/field-autocomplete/__init__.py
+++ src/field-autocomplete/__init__.py
@@ -1,9 +1,7 @@
-from .addon_manager import init_addon_manager
 from .editor import init_editor
 from .webview import init_webview
 from .dialog import init_settings_dialog
 
-init_addon_manager()
 init_editor()
 init_webview()
 init_settings_dialog()
