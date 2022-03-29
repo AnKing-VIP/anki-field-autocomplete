94f12f530a4069a29d23f2ac604f3c3b4a3b7530
RisingOrange <jakub.fidler@protonmail.com>
2022-03-29 22:16:29 +0200

    Make images in dialogs show up

--- src/field-autocomplete/__init__.py
+++ src/field-autocomplete/__init__.py
@@ -1,7 +1,9 @@
-from .editor import init_editor
-from .webview import init_webview
 from .dialog import init_settings_dialog
+from .editor import init_editor
+from .gui.resources import initialize_qt_resources
+from .webview import init_webview
 
+initialize_qt_resources()
 init_editor()
 init_webview()
 init_settings_dialog()
